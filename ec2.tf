a3e5aa007e33acde02e1bd6397f3ebe87ac54fb3
manjiree20 <manjireechaware20@gmail.com>
2022-11-07 17:33:06 +0530

    report

--- ec2.tf
+++ ec2.tf
@@ -12,8 +12,8 @@ echo "Hello Word" > /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
-sudo yum install openscap-scanner
-sudo yum install scap-security-guide
+sudo yum install openscap-scanner -y
+sudo yum install scap-security-guidey -y
 sudo ls -1 /usr/share/xml/scap/ssg/content/ssg-*-ds.xml
 sudo oscap info /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
 sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_pci-dss --results-arf arf.xml --report $HOSTNAME-report.html  /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
