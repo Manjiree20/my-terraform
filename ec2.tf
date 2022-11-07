ec8aec5a3c738095446144c5eac219f7d30a68bd
manjiree20 <manjireechaware20@gmail.com>
2022-11-07 13:51:41 +0530

    file added

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "demo_instance" {
+  ami           = "ami-0e6329e222e662a52"
+  instance_type = "t2.micro"
+  key_name = "terraform.pem"
+  user_data = <<EOF
+#!/bin/bash
+sudo yum install httpd -y
+echo "Hello Word" > /var/www/html/index.html
+sudo systemctl start httpd
+sudo systemctl enable httpd
+EOF
+}
