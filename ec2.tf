2ab42bc0296278f06e7803fc649a073ff42dc8a4
manjiree20 <manjireechaware20@gmail.com>
2022-11-09 15:52:36 +0530

    done

--- ec2.tf
+++ ec2.tf
@@ -7,15 +7,15 @@ resource "aws_instance" "demo_instance" {
   }
   user_data = <<EOF
 #!/bin/bash
- yum install httpd -y
- echo "Hello Word" > /var/www/html/index.html
- systemctl start httpd
- systemctl enable httpd
- amazon-linux-extras install epel -y
- yum install openscap-scanner -y
- yum install scap-security-guide -y
- ls -1 /usr/share/xml/scap/ssg/content/ssg-*-ds.xml
- oscap info /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
- oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_pci-dss --results-arf arf.xml --report $HOSTNAME-report.html  /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
-  EOF
-  }
+ sudo yum install httpd -y
+ sudo echo "Hello Word" > /var/www/html/index.html
+ sudo systemctl start httpd
+ sudo systemctl enable httpd
+ sudo amazon-linux-extras install epel -y
+ sudo yum install openscap-scanner -y
+ sudo yum install scap-security-guide -y
+ sudo ls -1 /usr/share/xml/scap/ssg/content/ssg-*-ds.xml
+ sudo oscap info /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
+ sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_pci-dss --results-arf arf.xml --report $HOSTNAME-report.html  /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
+EOF
+}
