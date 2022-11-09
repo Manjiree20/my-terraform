b6680b91e7cc5456f2c17cdfe85fc2d28fc471a3
manjiree20 <manjireechaware20@gmail.com>
2022-11-09 13:18:45 +0530

    done

--- ec2.tf
+++ ec2.tf
@@ -7,11 +7,11 @@ resource "aws_instance" "demo_instance" {
   }
   user_data = <<EOF
 #!/bin/bash
-sudo amazon-linux-extras install epel -y
-sudo yum install openscap-scanner -y
-sudo yum install scap-security-guide -y
-sudo ls -1 /usr/share/xml/scap/ssg/content/ssg-*-ds.xml
-sudo oscap info /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
-  sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_pci-dss --results-arf arf.xml --report $HOSTNAME-report.html  /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
+ amazon-linux-extras install epel -y
+ yum install openscap-scanner -y
+ yum install scap-security-guide -y
+ ls -1 /usr/share/xml/scap/ssg/content/ssg-*-ds.xml
+ oscap info /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
+ oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_pci-dss --results-arf arf.xml --report $HOSTNAME-report.html  /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
   EOF
   }
