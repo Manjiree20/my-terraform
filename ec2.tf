c08886917f94bc4b291415d81b119c21ba056e2e
manjiree20 <manjireechaware20@gmail.com>
2022-11-11 13:21:08 +0530

    done

--- ec2.tf
+++ ec2.tf
@@ -4,10 +4,12 @@ resource "aws_instance" "demo_instance" {
   key_name = "aws-tf"
   user_data = <<-EOF
  #!/bin/bash
- sudo yum install httpd -y
- echo "Hello Word" > /var/www/html/index.html
- sudo systemctl start httpd
- sudo systemctl enable httpd
+ sudo amazon-linux-extras install epel -y
+ sudo yum install openscap-scanner -y
+ sudo yum install scap-security-guide -y
+ sudo ls -1 /usr/share/xml/scap/ssg/content/ssg-*-ds.xml
+ sudo oscap info /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
+ sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_pci-dss --results-arf arf.xml --report $HOSTNAME-report.html  /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
 
 EOF
 tags = {
