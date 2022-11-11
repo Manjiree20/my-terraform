30eaccd6c337fa1935724ae2a77013de81ff6923
manjiree20 <manjireechaware20@gmail.com>
2022-11-11 15:57:49 +0530

    shellscript

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,13 @@
+data "template_file" "web-userdata"{
+  template = file("install_openscap.sh")
+}
+
+
 resource "aws_instance" "demo_instance" {
   ami           = "ami-0e6329e222e662a52"
   instance_type = "t2.micro"
   key_name = "aws-tf"
+  user_data = data.template_file.web-userdata.rendered
 tags = {
     Name = "terraform"
   }
