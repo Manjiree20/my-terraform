15f33e3e361fa4d5e531d258f4ebb489b18c9b3a
manjiree20 <manjireechaware20@gmail.com>
2022-11-14 16:06:38 +0530

    done

--- ec2.tf
+++ ec2.tf
@@ -12,8 +12,11 @@ resource "aws_instance" "demo_instance" {
  systemctl start httpd
  systemctl enable httpd
  amazon-linux-extras install epel -y
- sudo yum install libreswan
- sudo yum install aide
+ sudo yum install libreswan -y
+ sudo yum install aide -y
  sudo echo "05 4 * * * root /usr/sbin/aide --check" >> /etc/crontab
+ sudo /usr/sbin/aide --init
+ sudo cp /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
+ sudo /usr/sbin/aide --check
   EOF
   }
