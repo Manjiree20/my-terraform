972d997f3ec5e201c71cd5d9bdf8b14e6a902c01
manjiree20 <manjireechaware20@gmail.com>
2022-11-14 11:59:35 +0530

    done

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,24 @@
-data "template_file" "web-userdata"{
-  template = file("install_openscap.sh")
-}
-
-
 resource "aws_instance" "demo_instance" {
   ami           = "ami-0e6329e222e662a52"
   instance_type = "t2.micro"
   key_name = "aws-tf"
-  user_data = data.template_file.web-userdata.rendered
-tags = {
+  tags = {
     Name = "terraform"
   }
-}
+  user_data = <<EOF
+#!/bin/bash
+ yum install httpd -y
+ echo "Hello Word" > /var/www/html/index.html
+ systemctl start httpd
+ systemctl enable httpd
+ amazon-linux-extras install epel -y
+ yum install openscap-scanner -y
+ yum install scap-security-guide -y
+ ls -1 /usr/share/xml/scap/ssg/content/ssg-*-ds.xml
+ oscap info /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
+ oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_pci-dss --results-arf arf.xml --report $HOSTNAME-report.html  /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
+ sudo yum install libreswan
+ sudo yum install aide
+ sudo echo "05 4 * * * root /usr/sbin/aide --check" >> /etc/crontab
+  EOF
+  }
