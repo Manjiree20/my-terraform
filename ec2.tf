e0d1fc7e090b45e41b966921bfa72cd4f0460294
manjiree20 <manjireechaware20@gmail.com>
2022-11-14 12:40:57 +0530

    done

--- ec2.tf
+++ ec2.tf
@@ -12,11 +12,6 @@ resource "aws_instance" "demo_instance" {
  systemctl start httpd
  systemctl enable httpd
  amazon-linux-extras install epel -y
- yum install openscap-scanner -y
- yum install scap-security-guide -y
- ls -1 /usr/share/xml/scap/ssg/content/ssg-*-ds.xml
- oscap info /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
- oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_pci-dss --results-arf arf.xml --report $HOSTNAME-report.html  /usr/share/xml/scap/ssg/content/ssg-amzn2-ds.xml
  sudo yum install libreswan
  sudo yum install aide
  sudo echo "05 4 * * * root /usr/sbin/aide --check" >> /etc/crontab
